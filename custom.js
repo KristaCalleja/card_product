0afcaa3c13ed0673da3b97b305959b80419bf699
KristaCalleja <kristacm@gmail.com>
2021-04-08 19:33:41 -0300

    Re position footer-overlay in dom

--- custom.js
+++ custom.js
@@ -1,7 +1,5 @@
-// Create div element
-let div = document.createElement('div');
-// Set its class to .footer-overlay
-div.className = '.footer-overlay';
+// Select footer-overlay
+let footerOverlay = document.querySelector('.footer-overlay');
 
 // Select button for assigning handler
 let button = document.querySelector('.footer__icon');
@@ -10,7 +8,7 @@ let button = document.querySelector('.footer__icon');
 let footer = document.querySelector('.footer');
 
 // Function to assign handler using a DOM property on event
-button.onclick = function(){
+button.addEventListener('click', function(){
 // Special method 'replaceWith' to replace node with given node
-    footer.replaceWith(div);
-};
+    footerOverlay.classList.toggle('active');
+});
