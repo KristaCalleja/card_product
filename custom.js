def5f35099ba2111792e28fbffbff64151ab5fe2
KristaCalleja <kristacm@gmail.com>
2021-04-08 15:33:01 -0300

    Add onclick event

--- custom.js
+++ custom.js
@@ -1,8 +1,16 @@
 // Create div element
 let div = document.createElement('div');
-// Select footer from HTML
-let footer = document.querySelector('.footer');
 // Set its class to .footer-overlay
 div.className = '.footer-overlay';
+
+// Select button for assigning handler
+let button = document.querySelector('.footer__icon');
+
+// Select footer from HTML
+let footer = document.querySelector('.footer');
+
+// Function to assign handler using a DOM property on event
+button.onclick = function(){
 // Special method 'replaceWith' to replace node with given node
-footer.replaceWith(div);
+    footer.replaceWith(div);
+};
