321daeb0646754873c237fbf29aa78d8a984c786
KristaCalleja <kristacm@gmail.com>
2021-04-09 17:18:43 -0300

    Footer overlay toggle

--- custom.js
+++ custom.js
@@ -1,19 +1,15 @@
 // Select footer-overlay
-let footerOverlayDesktop = document.querySelector('.footer-overlay__desktop');
-let footerOverlayMobile = document.querySelector('.footer-overlay__mobile');
-
+let footerOverlay = document.querySelector('.footer-overlay');
 // Select button for assigning handler
 let button = document.querySelector('.footer__icon');
-
 // Select footer from HTML
 let footer = document.querySelector('.footer');
 
 // Function to assign handler using a DOM property on event
 button.addEventListener('click', function(){
 // Special method 'replaceWith' to replace node with given node
-    footerOverlayDesktop.classList.toggle('active');
+    footerOverlay.classList.toggle('visible');
+    // footer.classList.toggle('display');
 });
 
-button.addEventListener('click', function(){
-    footer.replaceWith(footerOverlayMobile);
-});
+
